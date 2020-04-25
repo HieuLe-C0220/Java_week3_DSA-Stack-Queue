f8ed3cf4f8254eb8672d0d0e62a4c546ff1302e9
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-25 17:09:33 +0700

    done

---
+++ Array_Queue/src/MyQueueTest.java
@@ -0,0 +1,18 @@
+public class MyQueueTest {
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue(4);
+        queue.enqueue(4);
+        queue.dequeue();
+        queue.enqueue(26);
+        queue.enqueue(2);
+        queue.enqueue(56);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(24);
+        queue.dequeue();
+        queue.enqueue(98);
+        queue.enqueue(45);
+        queue.enqueue(95);
+        queue.enqueue(2020);
+    }
+}
