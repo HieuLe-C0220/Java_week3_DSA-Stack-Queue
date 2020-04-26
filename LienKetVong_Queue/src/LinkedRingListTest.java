cbc365b7e29e8028536b985a6b98cb7037bb259e
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-26 16:43:05 +0700

    done

---
+++ LienKetVong_Queue/src/LinkedRingListTest.java
@@ -0,0 +1,13 @@
+public class LinkedRingListTest {
+    public static void main(String[] args) {
+        LinkedRingList circle = new LinkedRingList();
+        circle.enQueue(30);
+        circle.enQueue(18);
+        circle.enQueue(17);
+        circle.deQueue();
+        circle.deQueue();
+        circle.enQueue(13);
+        circle.enQueue(21);
+        circle.display();
+    }
+}
